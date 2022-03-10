b5809e5cf5b7b5ecc30301041e8daa3705572bf2
drewburns <drewburnsbab@gmail.com>
2022-03-09 19:21:22 -0500

    cors again

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ var cors = require("cors");
 var logger = require("morgan");
 
 const app = express();
-app.use(
-  cors({
-    allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
-    exposedHeaders: ["authorization"], // you can change the headers
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-  })
-);
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 
 var indexRouter = require("./routes/index");
